f187db2fa528ca9154416c0adf96351c2bb5e0d0
tranquilino28 <ntnfrias@gmail.com>
2026-04-25 20:43:32 -0500

    COMMIT VERSION 0.0.19 SE AÑADIO LA ENTIDAD DE ROLEASIGMENT PARA LOS CONTEXTOS Y EL STAF

--- src/main/java/org/frias/avalon/domain/user/infraestruture/persistence/entity/RoleAssignment.java
+++ src/main/java/org/frias/avalon/domain/user/infraestruture/persistence/entity/RoleAssignment.java
@@ -1,4 +1,50 @@
 package org.frias.avalon.domain.user.infraestruture.persistence.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Table
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class RoleAssignment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private Long roleId;
+
+    private Long staffScope;
+
+    private Long scope;
+
+   // private Long schedule;
+
+    private Long status;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+
+
 }
