ea2abc68a47c17f94216e4fde373f8afa680cae5
tranquilino28 <ntnfrias@gmail.com>
2026-07-11 10:09:51 -0500

    COMMIT VERSION 0.0.49 correccion del test de sales

--- src/test/java/org/frias/avalon/domain/sale/application/usecase/sale/create/CreateSaleUseCaseImplTest.java
+++ src/test/java/org/frias/avalon/domain/sale/application/usecase/sale/create/CreateSaleUseCaseImplTest.java
@@ -19,6 +19,8 @@ import org.frias.avalon.domain.sale.domain.SaleDomain;
 import org.frias.avalon.domain.sale.domain.service.SaleWeightConversionService;
 import org.frias.avalon.domain.user.domain.model.UserAvalonDomain;
 import org.frias.avalon.domain.user.domain.port.UserAvalonRepositoryPort;
+import org.frias.avalon.domain.credit.application.port.CreditRepositoryPort;
+import org.springframework.context.ApplicationEventPublisher;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -47,6 +49,8 @@ class CreateSaleUseCaseImplTest {
     @Mock private MasterTreeProvider masterTreeProvider;
     @Mock private SaleWeightConversionService weightConversionService;
     @Mock private CurrentUserProviderPort currentUserProvider;
+    @Mock private CreditRepositoryPort creditRepositoryPort;
+    @Mock private ApplicationEventPublisher eventPublisher;
 
     @InjectMocks
     private CreateSaleUseCaseImpl useCase;
@@ -71,7 +75,7 @@ class CreateSaleUseCaseImplTest {
         when(masterTreeProvider.getTree()).thenReturn(masterTree);
         MasterRoot roleNode = new MasterRoot(10L, "CAJTUR", "Cajero Turno", 100L, 1L);
         when(masterTree.getByCode("CAJTUR")).thenReturn(roleNode);
-        when(masterTree.isChildOf(roleNode, "OPERACION")).thenReturn(true);
+        when(masterTree.isChildOf(roleNode, "OPT")).thenReturn(true);
 
         // Mocking Employee resolving
         UserAvalonDomain user = UserAvalonDomain.fromPersistenceBasic(1L, 20L, "employee_user", 1L);
@@ -117,7 +121,8 @@ class CreateSaleUseCaseImplTest {
         // Request definition
         CreateSaleRequest request = new CreateSaleRequest(
                 "123456", 4L, 8L, new BigDecimal("10"),
-                List.of(new SaleItemRequest(50L, "1.5"))
+                List.of(new SaleItemRequest(50L, "1.5")),
+                false
         );
 
         SaleResponse response = useCase.execute(request);
@@ -141,7 +146,8 @@ class CreateSaleUseCaseImplTest {
         // Requesting for outlet 5L, but current employee is tied to 4L
         CreateSaleRequest request = new CreateSaleRequest(
                 "123456", 5L, 8L, new BigDecimal("10"),
-                List.of(new SaleItemRequest(50L, "1.5"))
+                List.of(new SaleItemRequest(50L, "1.5")),
+                false
         );
 
         assertThrows(BusinessException.class, () -> useCase.execute(request));
